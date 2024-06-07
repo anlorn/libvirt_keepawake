c009fa2e8bb0f278d84f81575a456f55d353dd08
Anton Tinyakov <anlorn@anlorn.com>
2024-06-06 23:02:10 -0400

    Log to stderr

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	log "github.com/sirupsen/logrus"
 	"libvirt_keepawake/cmd"
+	"os"
 )
 
 func main() {
+	// output to stdout instead of the default stderr
+	log.SetOutput(os.Stdout)
 	cmd.Execute()
 }
